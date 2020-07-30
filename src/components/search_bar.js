c22a4d72a6c46508d480016d624a66a12c3d7940
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-30 12:18:12 +0530

    added the state manipulation

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input
-      onChange={(event) => console.log(event.target.value)}
-    />;
+    return (
+      <div>
+        <input
+          onChange={(event) => this.setState({ term: event.target.value })}
+        />
+        value of the term: {this.state.term}
+      </div>);
   }
 }
 
