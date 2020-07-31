a33224b9f2842e081dcfef104a11bbae0d81822d
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-31 19:12:22 +0530

    rename

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ class App extends Component {
 
         this.state = { videos: [] }
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
-            this.setState({ videos: data });
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos: videos });
         });
     }
 
