c99cb40c1138ce3920097d590e583aaeb07b47c3
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-31 19:11:57 +0530

    refactoring to class based componenets

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 //create a new component to produce some HTML
-import ReactDOM from "react-dom"
-import React from "react"
-
-import SearchBar from './components/search_bar'
-
-// Youtube API key
+import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 const API_KEY = "AIzaSyD3a2tscmqaZ10nz44BXnmdMrckNXaUkGM";
 
-let App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] }
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+            this.setState({ videos: data });
+        });
+    }
+
+    render() {
+        return <div>
+            <SearchBar />
+        </div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
