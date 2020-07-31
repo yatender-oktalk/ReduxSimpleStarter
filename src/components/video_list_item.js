76bcf1c8432cc4e5974be81f19231a8db98c4cdd
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-31 20:26:29 +0530

    setting up the selected video

---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// es6 syntax to fetch video from the props kind of pattern matching
+// in functional programming
+const VideoListItem = ({ video }) => {
+    // const video = props.video;
+    const imgUrl = video.snippet.thumbnails.default.url;
+
+    return <li className="list-group-item">
+
+        <div className="video-list-media">
+            <div className="media-left">
+                <img className="media-object" src={imgUrl} />
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </div>
+    </li>
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -3,22 +3,33 @@ import ReactDOM from "react-dom";
 import React, { Component } from "react";
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
+
 const API_KEY = "AIzaSyD3a2tscmqaZ10nz44BXnmdMrckNXaUkGM";
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] }
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
 
         YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render() {
         return <div>
             <SearchBar />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList videos={this.state.videos} />
         </div>
     }
 }
