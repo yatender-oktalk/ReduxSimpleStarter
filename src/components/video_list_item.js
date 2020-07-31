00f6ef9792c8c6d5bc0b86599067b71fb2faf524
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-31 20:31:55 +0530

    updating the on click

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 // es6 syntax to fetch video from the props kind of pattern matching
 // in functional programming
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
     // const video = props.video;
     const imgUrl = video.snippet.thumbnails.default.url;
 
-    return <li className="list-group-item">
+    return <li onClick={() => onVideoSelect(video)} className="list-group-item">
 
         <div className="video-list-media">
             <div className="media-left">
