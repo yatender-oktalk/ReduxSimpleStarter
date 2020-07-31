9d61891d47634c3bf1314941e281778cd969a5ba
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-31 20:49:25 +0530

    adding search term

--- src/index.js
+++ src/index.js
@@ -5,35 +5,40 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
+import { template } from "lodash";
 
 const API_KEY = "AIzaSyD3a2tscmqaZ10nz44BXnmdMrckNXaUkGM";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        }
-
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
+    this.state = {
+      videos: [],
+      selectedVideo: null
     }
 
-    render() {
-        return <div>
-            <SearchBar />
-            <VideoDetail video={this.state.selectedVideo} />
-            <VideoList
-                onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-                videos={this.state.videos} />
-        </div>
-    }
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return <div>
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        videos={this.state.videos} />
+    </div>
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
