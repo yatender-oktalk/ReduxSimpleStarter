288d381c29a0532d46b96aa6a93dcf27d8188d07
yatender-oktalk <yatender.nitk@gmail.com>
2020-07-31 20:58:21 +0530

    formatting

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,21 +3,21 @@ import React from 'react'
 // es6 syntax to fetch video from the props kind of pattern matching
 // in functional programming
 const VideoListItem = ({ video, onVideoSelect }) => {
-    // const video = props.video;
-    const imgUrl = video.snippet.thumbnails.default.url;
+  // const video = props.video;
+  const imgUrl = video.snippet.thumbnails.default.url;
 
-    return <li onClick={() => onVideoSelect(video)} className="list-group-item">
+  return <li onClick={() => onVideoSelect(video)} className="list-group-item">
 
-        <div className="video-list-media">
-            <div className="media-left">
-                <img className="media-object" src={imgUrl} />
-            </div>
+    <div className="video-list-media">
+      <div className="media-left">
+        <img className="media-object" src={imgUrl} />
+      </div>
 
-            <div className="media-body">
-                <div className="media-heading">{video.snippet.title}</div>
-            </div>
-        </div>
-    </li>
+      <div className="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+      </div>
+    </div>
+  </li>
 };
 
 export default VideoListItem;
